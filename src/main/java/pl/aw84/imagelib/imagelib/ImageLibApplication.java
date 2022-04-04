9302543b67a6e1c46729edaf9fe0e9007c5b057b
aw84 <silencewalker@gmail.com>
2022-04-04 23:06:13 +0200

    init

---
+++ src/main/java/pl/aw84/imagelib/imagelib/ImageLibApplication.java
@@ -0,0 +1,13 @@
+package pl.aw84.imagelib.imagelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/aw84/imagelib/imagelib/ImageLibApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aw84.imagelib.imagelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
