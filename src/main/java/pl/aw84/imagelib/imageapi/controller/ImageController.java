e66f53ac15ac22a3b5bd6aff439e3a72e1ca5f4d
aw84 <silencewalker@gmail.com>
2022-04-10 11:20:13 +0200

    prop refresh

--- src/main/java/pl/aw84/imagelib/imageapi/controller/ImageController.java
+++ src/main/java/pl/aw84/imagelib/imageapi/controller/ImageController.java
@@ -1,9 +1,11 @@
 package pl.aw84.imagelib.imageapi.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RefreshScope
 @RestController
 public class ImageController {
     @Value("${greeting}")
