29171109397cde611dca0bd038ab64429019ab12
aw84 <silencewalker@gmail.com>
2022-04-16 21:57:49 +0200

    sleuth+zipkin

--- src/main/java/pl/aw84/imagelib/imageapi/ImageApiApplication.java
+++ src/main/java/pl/aw84/imagelib/imageapi/ImageApiApplication.java
@@ -5,6 +5,9 @@ import java.security.Security;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @SpringBootApplication
 public class ImageApiApplication {
@@ -13,6 +16,11 @@ public class ImageApiApplication {
 		Security.setProperty("crypto.policy", "unlimited");
 		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(ImageApiApplication.class, args);
+	}
+
+	@Bean
+	WebClient imageApiWebClient() {
+		return WebClient.create();
 	}
 
 }
